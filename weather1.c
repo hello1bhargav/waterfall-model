6cf511331bfff139e657e9018596cf3d60feb3fa
Bhargav <bhargavodf@gmail.com>
2025-03-10 09:22:27 +0530

    static model

--- weather1.c
+++ weather1.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+double calculate_weather(double t, double h, double w) {
+    return 0.5 * t * t - 0.2 * h + 0.1 * w - 15;
+}
 int main()
 {
     int t, h, w;
@@ -6,8 +9,6 @@ int main()
     h=50;
     w=10;
     double W = calculate_weather(t, h, w);
-    printf("")
-}
-double calculate_weather(double t, double h, double w) {
-    return 0.5 * t * t - 0.2 * h + 0.1 * w - 15;
+    printf("Weather: %.2f\n", W);
+    return 0;
 }
